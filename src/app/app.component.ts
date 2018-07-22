62625ecbbcda81b8cacea57d7d088a5914d96dce
WanchaloemMacBookPro <wanchaloemmacbookpro@WanchaloemMacBookPros-MacBook-Pro.local>
2018-07-22 08:49:10 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,6 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    positions = [
-        'โปรแกรมเมอร์',
-        'นักเคราะห์ระบบ',
-        'นักออกแบบ'
-    ];
+    positions = ['โปรแกรมเมอร์', 'นักเคราะห์ระบบ', 'นักออกแบบ'];
     constructor() { }
 }
